cdea622d05ed5af5d9675f614e112f01bbd71de7
chiharuszalay <chiharu.szalay@gmail.com>
2015-09-21 10:56:41 -0400

    Detecting List Selections

--- HW3/app/src/main/java/com/example/chiharu/hw3/MainActivity.java
+++ HW3/app/src/main/java/com/example/chiharu/hw3/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.chiharu.hw3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,7 +15,8 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+AdapterView.OnItemClickListener {
 
     TextView mainTextView;
     Button mainButton;
@@ -50,6 +53,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Set the ListView to use the ArrayAdapter
         mainListView.setAdapter(mArrayAdapter);
+
+        // 5. Set this activity to react to list items being pressed
+        mainListView.setOnItemClickListener(this);
     }
 
     @Override
@@ -69,5 +75,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Also add that value to the list shown in the ListView
         mNameList.add(mainEditText.getText().toString());
         mArrayAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        // Log the item's position and contents
+        // to the console in Debug
+        Log.d("omg android", position + ": " + mNameList.get(position));
     }
 }
